996f0d9f4ce49e98d75535c5425568539036e49d
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-08-21 13:05:45 -0400

    Styled Landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,8 +32,25 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
-      <div className="view-more-data-btn-container">
+      <div className="graphs-section">
+        <div className='graph-col'>
+
+        <img src={GrantRatesByOfficeImg} alt='Grant Rates By Office'className='office-img'/>
+        <p>Search Grant Rates By Office</p>
+        </div>
+        <div className='graph-col'>
+
+        <img src={GrantRatesByNationalityImg} alt='Grant Rates By Nationality'className='nationality-img'/>
+        <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className='graph-col'>
+
+        <img src={GrantRatesOverTimeImg} alt='Grant Rates Over Time'className='over-time-img'/>
+        <p>Search Grant Rates Over Time</p>
+        </div>
+
+        </div>
+      <div className="data-btn-container">
         <Button
           type="default"
           style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
@@ -41,7 +58,15 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/notfound')}
+        >
+          Download the Data
+        </Button>
       </div>
+      
 
       <div className="middle-section">
         <div className="hrf-img-container">
@@ -55,13 +80,61 @@ function RenderLandingPage(props) {
             through a Freedom of Information Act request. You can search for
             information on asylum grant rates by year, nationality, and asylum
             office, visualize the data with charts and heat maps, and download
-            the data set
+            the data set.
           </h3>
         </div>
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1 className='bottom-header'>
+            Systemic Disparity Insights
+          </h1>
+          <div className='data-text'>
+            <div>
+              <h1>
+                36%
+              </h1>
+              <p>
+                By the end of the Trump administration, the average asylum
+                 office grant rate had fallen 36 precent from an average of 
+                 44 percent in fiscal year 2016 to 28 precent in fiscal year 
+                 2020.
+                 </p>
+            </div>
+            <div>
+              <h1>
+                5%
+              </h1>
+              <p>
+                The New York asylum office grant rate dropped to 6 percent
+                in fiscal year 2020.
+
+              </p>
+            </div>
+            <div>
+              <h1>
+                6x Lower
+              </h1>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                 average grant rate was six times lower than the San Francisco 
+                 asylum office.
+
+              </p>
+            </div>
+          </div>
+          </div>
+          <div className='read-more'>
+
+          <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/notfound')}
+          >
+          Read More
+        </Button>
+          </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
